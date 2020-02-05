90e46bf984c6648e56510aa8c1c5df2aede40014
ChrisMuir <chrismuirRVA@gmail.com>
2020-02-04 21:50:38 -0500

    remove unused function

--- R/utils.R
+++ R/utils.R
@@ -26,15 +26,3 @@ get_addr_query_uri <- function(lon, lat) {
      "&output=json&pois=0"
   )
 }
-
-
-#' Get coords from an API uri
-#'
-#' @noRd
-get_coords_from_uri <- function(uri) {
-  uri <- strsplit(uri, "location=", fixed = TRUE)[[1]][2]
-  uri <- strsplit(uri, ",", fixed = TRUE)
-  lat <- as.double(uri[[1]][1])
-  lon <- as.double(strsplit(uri[[1]][2], "&", fixed = TRUE)[[1]][1])
-  c("lat" = lat, "lon" = lon)
-}
